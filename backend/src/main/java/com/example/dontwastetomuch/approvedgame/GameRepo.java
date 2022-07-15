429f466fa0494c1ae29bd4b873662eec0f869af2
chris <chrisweber1991@gmail.com>
2022-07-15 09:34:28 +0200

    fix save games

--- backend/src/main/java/com/example/dontwastetomuch/approvedgame/GameRepo.java
+++ backend/src/main/java/com/example/dontwastetomuch/approvedgame/GameRepo.java
@@ -3,8 +3,6 @@ package com.example.dontwastetomuch.approvedgame;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 
 @Repository
 public interface GameRepo extends MongoRepository<Game, String> {
