ef631a6e9191da448b916e852f3e84a01c0ddcfd
chris <chrisweber1991@gmail.com>
2022-07-20 15:24:45 +0200

    ci

--- frontend/src/Pages/InfoPage.tsx
+++ frontend/src/Pages/InfoPage.tsx
@@ -1,6 +1,6 @@
 import {useParams} from "react-router-dom";
 import {useCallback, useEffect, useState} from "react";
-import {editGame, getOneGame, postToMyGames} from "../service/apiService";
+import {editGame, getOneGame} from "../service/apiService";
 
 
 export default function InfoPage(){
@@ -8,7 +8,6 @@ export default function InfoPage(){
     const {id} = useParams()
     const [gameName, setGameName] = useState(0)
     const [errorMessageId, setErrorMessageId] = useState("")
-    const [errorMessageFav, setErrorMessageFav] = useState("")
 
 
     const fetchGame = useCallback( ()=>{
@@ -34,12 +33,6 @@ export default function InfoPage(){
                 .then(fetchGame)
     }
 
-    function addToMyGames(){
-        if(id){
-            postToMyGames(id)
-                .catch(()=> setErrorMessageFav("Game already added to your list"))
-        }
-    }
 
     return(
         <div>
@@ -48,7 +41,6 @@ export default function InfoPage(){
             </div>
             <div>
                 {errorMessageId && <div>{errorMessageId}</div>}
-                {errorMessageFav && <div>{errorMessageFav}</div>}
                 <div>{gameName}</div>
                 <div>
                     <input type={"number"}/><button>Add</button>
