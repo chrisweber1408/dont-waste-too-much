2d3628fdd9477c37fec7d31932a2fe86aad99a5e
chris <chrisweber1991@gmail.com>
2022-07-29 16:40:03 +0200

    Heroku

--- backend/src/main/java/com/example/dontwastetomuch/ReactRoutingForwarding.java
+++ backend/src/main/java/com/example/dontwastetomuch/ReactRoutingForwarding.java
@@ -1,4 +1,4 @@
-package com.example.dontwastetomuch.security;
+package com.example.dontwastetomuch;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
